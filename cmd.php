15afcaed8716c83a4bf6bf4ef2b09e8e5478605d
Tyler Hadidon <hadidotj@gmail.com>
2018-11-06 17:42:37 +0000

    Initial commit for web!

---
+++ cmd.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+
+$s=fsockopen('192.168.1.3',44147,$errnum,$errstr,5) ;
+if($s!==FALSE) {
+	fwrite($s, $_GET['cmd']);
+    while(!feof($s)) {
+        echo fgets($s, 128);
+    }
+    fclose($s);
+} else {
+	http_response_code(500);
+	echo '{"status":500,"err":"Failed to connect to thermostat server: ' . trim($errstr) . '"}';
+}
+?>
