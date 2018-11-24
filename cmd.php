5d7efb014b14d6d4d75d9ca45b3056b27312142a
Tyler Hadidon <hadidotj@gmail.com>
2018-11-24 16:35:55 +0000

    Okay, trying yet another way to close the sockets...

--- cmd.php
+++ cmd.php
@@ -1,18 +1,17 @@
 <?php
 header('Content-Type: application/json');
 
-$s = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if($s !== FALSE && socket_connect($s, 'localhost', 44147) !== FALSE) {
-	$in = $_GET['cmd'];
-	socket_write($s, $in, strlen($in));
-	
-	while(($out = socket_read($s, 2048)) !== FALSE) {
-		echo $out;
-	}
-	
-	socket_close($s);
+$s=stream_socket_client('tcp://127.0.0.1:44147',$errnum,$errstr,5) ;
+if($s!==FALSE) {
+	fwrite($s, $_GET['cmd']);
+    while(!feof($s)) {
+        echo fgets($s, 128);
+    }
+	stream_socket_shutdown($s,STREAM_SHUT_RDWR)
+	sleep(1)
+    fclose($s);
 } else {
 	http_response_code(500);
-	echo '{"status":500,"err":"Failed to connect to thermostat server: ' . trim(socket_strerror(socket_last_error($s))) . '"}';
+	echo '{"status":500,"err":"Failed to connect to thermostat server: ' . trim($errstr) . '"}';
 }
 ?>
